f7cc264ad867cab99eeb20cb576a67b7fedc4e2c
mfv1999 <manuel.flores1@alu.uclm.es>
2022-12-02 19:19:14 +0100

    commit 16 diciembre 19:19

--- iceflix/cli.py
+++ iceflix/cli.py
@@ -1,9 +1,14 @@
 """Submodule containing the CLI command handlers."""
 
 import logging
+
 import sys
+import Ice 
 
 from iceflix.main import MainApp
+from typing import Self
+from os import system
+
 
 
 def setup_logging():
@@ -36,8 +41,39 @@ def authentication_service():
     sys.exit(0)
 
 
-def client():
+def client(self):
     """Handles the IceFlix client CLI command."""
+    """ primero el cliente pide el proxy del main a usar , despues da opciones a realizar """
     print("Starting IceFlix client...")
-    print("this is a test using git")
+
+    print("iniciando sistema")
+
+    """ pedir proxy del main """
+
+    """ opciones una vez iniciado con el proxy bien """
+
+    while 1:
+        print("Opciones disponibles")
+        print("1. Autenticarse")
+        print("2. buscar en el catalogo")
+        print("3. edicion del catalogo")
+        print("4 reproducir medios")
+        print("5 operaciones administrativas")
+        print("6. cerrar sesión")
+
+
+        opcion= input(self.create_prompt("MainService"))
+        while not opcion.isdigit() or int(opcion)<1 or int(opcion)> 6:
+            if (opcion== ""):
+                opcion= input(self.create_prompt("MainService"))     
+            else:
+                opcion = input("Inserta una opcion valida:")
+
+        if opcion == 1:
+            """ settup loggin o autenticator ?""" 
+        elif opcion ==2:
+            self.catalog_service()
+        elif opcion == 3:
+            """ catalog again ?"""
+
     sys.exit(0)
